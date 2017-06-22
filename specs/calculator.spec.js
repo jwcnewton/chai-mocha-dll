a3afdf6a94b245080d9f1a78c91e36ea0ee4738c
jwcnewton <jwcnewton@gmail.com>
2017-06-22 23:02:20 +0100

    Added safe addition function with tests

--- specs/calculator.spec.js
+++ specs/calculator.spec.js
@@ -1,16 +1,50 @@
-const shopper = require('../src/calculator');
-const should = require('chai').should();
+const chai = require('chai');
+const expect = chai.expect;
+const should = chai.should();
+const calculator = require('../src/calculator');
 
 describe('Add tests', function () {
     it('Add returns the correct value', function () {
-        let result = shopper.add(1, 2);
+        let result = calculator.add(1, 2);
         result.should.equal(3);
     });
 });
 
-describe('Subtract tests', function(){
-    it('Subtract returns the correct value', function(){
-        let result = shopper.subtract(10, 5);
+describe('Subtract tests', function () {
+    it('Subtract returns the correct value', function () {
+        let result = calculator.subtract(10, 5);
         result.should.equal(5);
+    });
+});
+
+describe('safeAddition generated tests positive parameters', function () {
+    let specs = [
+        {params: {a: 3, b: 2}, expected: 5},
+        {params: {a: 10, b: 2}, expected: 12},
+        {params: {a: 122, b: 78}, expected: 200},
+        {params: {a: 232, b: 7}, expected: 239}
+    ];
+
+    specs.forEach(function (spec) {
+        it(`correctly adds ${spec.params.a} to ${spec.params.b}`, function () {
+            var result = calculator.safeAddition(spec.params.a, spec.params.b);
+            result.should.equal(spec.expected);
+        });
+    });
+});
+
+describe('safeAddition generated tests nagative parameters', function () {
+    let specs = [
+        {params: {a: null, b: 2}, expected: 'null is not a number'},
+        {params: {a: undefined, b: 2}, expected: 'undefined is not a number'},
+        {params: {a: 23, b: 'acd'}, expected: 'acd is not a number'}
+    ];
+
+    specs.forEach(function (spec) {
+        it(`correctly adds ${spec.params.a} to ${spec.params.b}`, function () {
+            expect(function(){
+                calculator.safeAddition(spec.params.a, spec.params.b);
+            }).to.throw(spec.expected);
+        });
     });
 });
--- src/calculator.js
+++ src/calculator.js
@@ -6,4 +6,20 @@ let subtract = (a, b) => {
   return a - b;
 };
 
-module.exports = {add, subtract};
+let safeAddition = (x, y) => {
+  try {
+    isNumberValidation(y);
+    isNumberValidation(x);
+    return x + y;
+  } catch (err) {
+    throw err;
+  }
+};
+
+function isNumberValidation(numberUT) {
+  if (isNaN(parseInt(numberUT))) {
+    throw `${numberUT} is not a number`;
+  }
+};
+
+module.exports = {add, subtract, safeAddition};
