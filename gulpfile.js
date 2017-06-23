6b3beb65af3009819a4ef20be0d4752ceccdd2ac
Jonathan Newton <jwcnewton@gmail.com>
2017-06-23 08:39:33 +0100

    Fixed lint issue on gulp

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ const gulp = require('gulp');
 const sourcemaps = require('gulp-sourcemaps');
 const babel = require('gulp-babel');
 const concat = require('gulp-concat');
- 
+
 gulp.task('default', () => {
     return gulp.src('src/**/*.js')
         .pipe(sourcemaps.init())
